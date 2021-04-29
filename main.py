bdb51a1830463713c6caffeea31a5887be8b5190
shenzebang <shenzebang@gmail.com>
2021-04-29 12:39:39 -0400

    add train_critic

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import jax.numpy as jnp
+from jax.tree_util import tree_structure
 import jax.random as random
 import objax
+from objax.variable import TrainVar
 import jax
+from jax import jvp, grad
+from jax.experimental.ode import odeint
 import numpy as np
 import torch
 
@@ -31,7 +35,7 @@ num_workers = 8
 # load dataset with PyTorch
 transform = transforms.Compose([
     transforms.ToTensor(),
-    # transforms.Normalize((0.1307,), (0.3081,))
+    transforms.Normalize((0.1307,), (0.3081,))
 ])
 dataset = {
     "train": MNIST(os.path.join(DATA_DIR, "mnist"), train=True, transform=transform, download=True),
@@ -47,44 +51,38 @@ marginal_prob_std_fn = lambda t: marginal_prob_std(t, sigma)
 diffusion_coeff_fn = lambda t: diffusion_coeff(t, sigma)
 
 
-
-
-# net = ScoreNet(512)
-key = random.PRNGKey(3)
-key, subkey = random.split(key)
-net = UNet(marginal_prob_std_fn, subkey)
-
-
-def score_matching_loss_fn(x, key):
-    key, subkey = random.split(key)
-    random_t = random.uniform(subkey, [x.shape[0]])
-    key, subkey = random.split(key)
-    z = random.normal(subkey, x.shape)
-    std = marginal_prob_std_fn(random_t)
-    perturbed_x = x + z * std[:, None, None, None]
-    score = net(random_t, perturbed_x, training=True)
-    loss = jnp.mean(jnp.sum((score * std[:, None, None, None] + z) ** 2, axis=(1, 2, 3)))
-    return loss
-
-
-opt = objax.optimizer.Adam(net.vars())
-gv = objax.GradValues(score_matching_loss_fn, net.vars())
-
-
-def train_op(x, key):
-    g, v = gv(x, key)
-    opt(lr, g)
-    return v
-
-
-train_op_jit = objax.Jit(train_op, gv.vars() + opt.vars())
 # train_op_jit = train_op
 n_epoch = 500
 # batch_size = 32
 lr = 1e-4
-
+tolerance = 1e-3
 
 def train(key):
+    # key = random.PRNGKey(3)
+    key, subkey = random.split(key)
+    net = UNet(marginal_prob_std_fn, subkey)
+
+    def score_matching_loss_fn(x, key):
+        key, subkey = random.split(key)
+        random_t = random.uniform(subkey, [x.shape[0]])
+        key, subkey = random.split(key)
+        z = random.normal(subkey, x.shape)
+        std = marginal_prob_std_fn(random_t)
+        perturbed_x = x + z * std[:, None, None, None]
+        score = net(random_t, perturbed_x, training=True)
+        loss = jnp.mean(jnp.sum((score * std[:, None, None, None] + z) ** 2, axis=(1, 2, 3)))
+        return loss
+
+    opt = objax.optimizer.Adam(net.vars())
+    gv = objax.GradValues(score_matching_loss_fn, net.vars().subset(is_a=TrainVar))
+
+    def train_op(x, key):
+        g, v = gv(x, key)
+        opt(lr, g)
+        return v
+
+    train_op_jit = objax.Jit(train_op, gv.vars() + opt.vars())
+
     for epoch in trange(n_epoch):
         # training process
         avg_loss = 0.
@@ -101,12 +99,108 @@ def train(key):
     objax.io.save_var_collection(os.path.join(model_path, 'scorenet.npz'), net.vars())
 
 
+num_critic_epoch = 1
 
-def test(key):
+def train_critic(key):
+    key, subkey = random.split(key)
+    net = UNet(marginal_prob_std_fn, subkey)
+    critic = UNet(marginal_prob_std_fn, subkey)
+    objax.io.load_var_collection(os.path.join(model_path, 'scorenet.npz'), net.vars())
+    objax.io.load_var_collection(os.path.join(model_path, 'scorenet.npz'), critic.vars())
+
+    shape_list = [var.shape for var in critic.vars().subset(is_a=TrainVar)]
+    _reshape_like = lambda x: reshape_like(x, shape_list)
+    # define gradient and value, no need for adjoint system
+    def critic_gv_fn(x_init, key):
+        # define v for the Hutchinson’s Estimator
+        key, subkey = random.split(key)
+        v = random.normal(subkey, tuple([20]+list(x_init.shape)))
+        # define the initial states
+        t_0 = jnp.zeros(x_init.shape[0])
+        score_init = net(t_0, x_init, training=False)
+        critic_loss_init = jnp.zeros(1)
+        critic_grad_init = jnp.zeros_like(flatten(critic.vars().subset(is_a=TrainVar)))
+        state_init = [x_init, score_init, critic_loss_init, critic_grad_init]
+        def ode_func(states, t):
+            x = states[0]
+            score = states[1]
+            _t = jnp.ones([x.shape[0]]) * t
+            diffusion_weight = diffusion_coeff_fn(t)
+            score_pred = net(_t, x, training=False)
+            dx = -.5 * (diffusion_weight ** 2) * score_pred
+
+            f = lambda x: net(_t, x, training=False)
+            def divergence_fn(_x, _v):
+                # Hutchinson’s Estimator
+                # computes the divergence of net at x with random vector v
+                _, u = jvp(f, (_x,), (_v,))
+                return jnp.sum(jnp.dot(u, _v))
+            batch_div_fn = jax.vmap(divergence_fn, in_axes=[None, 0])
+            def batch_div(x):
+                return batch_div_fn(x, v).mean(axis=0)
+            grad_div_fn = grad(batch_div)
+
+            dscore_1 = - grad_div_fn(x)
+            dscore_2 = - jvp(f, (x,), (score,))[1] # f(x), df/dx * v = jvp(f, x, v)
+            dscore = dscore_1 + dscore_2
+
+            def dcritic_loss_fn(x):
+                critic_pred = critic(_t, x, training=True)
+                loss = ((critic_pred - score_pred) ** 2).sum(axis=(1, 2, 3)).mean()
+                return loss
+
+            dc_gv = objax.GradValues(dcritic_loss_fn, critic.vars())
+            dcritic_grad, dcritic_loss = dc_gv(x)
+            dcritic_grad = flatten(dcritic_grad)
+            dcritic_loss = dcritic_loss[0][None]
+            # print("loss ", dcritic_loss[0].shape, "grad", dcritic_grad.shape)
+            dstates = [dx, dscore, dcritic_loss, dcritic_grad]
+            return dstates
+
+        ode_func = jax.jit(ode_func)
+        tspace = np.array((0., 1.))
+
+        result = odeint(ode_func, state_init, tspace, atol=tolerance, rtol=tolerance)
+
+        return result[3][1], result[2][1]
+
+    # define optimizer
+    opt = objax.optimizer.Adam(critic.vars().subset(is_a=TrainVar))
+    critic_gv_fn_jit = jax.jit(critic_gv_fn)
+
+    def train_op(x, key):
+        g, v = critic_gv_fn_jit(x, key)
+        g = _reshape_like(g)
+        # change the shape of g!
+        opt(lr, g)
+        return v
+
+    # train_op_jit = jax.jit(train_op)
+    # define train_op
+
+    # training process
+    for epoch in range(num_critic_epoch):
+        avg_loss = 0.
+        num_data = 0
+        for _, (data, _) in enumerate(dataloader["train"]):
+            key, subkey = random.split(key)
+            data = jnp.array(data.numpy())
+            # _loss = train_op_jit(data, subkey)[0]
+            _loss = train_op(data, subkey)[0]
+            avg_loss += _loss
+            num_data += data.shape[0]
+            # print(avg_loss / num_data)
+        avg_loss /= num_data
+        print('Epoch %04d  Loss %.5f' % (epoch + 1, avg_loss))
+
+    objax.io.save_var_collection(os.path.join(model_path, 'critic.npz'), critic.vars())
+
+
+def test(key, ckpt_file):
     _key = random.PRNGKey(3)
     _key, _subkey = random.split(_key)
     net = UNet(marginal_prob_std_fn, _subkey)
-    objax.io.load_var_collection(os.path.join(model_path, 'scorenet.npz'), net.vars())
+    objax.io.load_var_collection(ckpt_file, net.vars())
     ## Load the pre-trained checkpoint from disk.
 
     sample_batch_size = 64  # @param {'type':'integer'}
@@ -134,7 +228,11 @@ def test(key):
 
 
 if __name__ == '__main__':
+    # key = random.PRNGKey(1)
+    # train(key)
     key = random.PRNGKey(1)
-    train(key)
+    train_critic(key)
     key = random.PRNGKey(2)
-    test(key)
+    # ckpt_file = os.path.join(model_path, 'scorenet.npz')
+    ckpt_file = os.path.join(model_path, 'critic.npz')
+    test(key, ckpt_file)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import objax
 
 
 def marginal_prob_std(t: float, sigma: float) -> float:
@@ -7,5 +8,25 @@ def marginal_prob_std(t: float, sigma: float) -> float:
 
 def diffusion_coeff(t: float, sigma: float) -> float:
     return sigma**t
+
+def flatten(vars: objax.VarCollection):
+    return jnp.concatenate([jnp.ravel(param) for param in vars], axis=0)
+
+def reshape_like(flat_tensor, shape_list):
+    prev_ind = 0
+    _tensors = []
+    for shape in shape_list:
+        # print(var.shape)
+        flat_size = jnp.prod(jnp.asarray(shape))
+        # print(shape)
+        # print(shape[0])
+        # flat_size = int(jnp.prod(shape[0]))
+        # flat_size = int(shape)
+        _tensors.append(flat_tensor[prev_ind:prev_ind+flat_size].reshape(shape))
+        prev_ind += flat_size
+
+    return _tensors
+
+
 
 

5ac4191ab7da2b39bb313766fded595be763f436
shenzebang <shenzebang@gmail.com>
2022-06-30 00:12:00 +0800

    remove unnecessary files

--- script/run_2D_Fokker_Planck.sh
+++ script/run_2D_Fokker_Planck.sh
@@ -4,4 +4,5 @@ python main_PINN.py --test_frequency 100 --learning_rate 2e-3 --PDE 2D-Fokker-Pl
 
 
 python main_COLT.py --test_frequency 100 --learning_rate 2e-3 --PDE 2D-Fokker-Planck --train_batch_size 64\
-            --total_evolving_time 3. --ODE_tolerance 1e-5 --plot_domain_size 10. --diffusion_coefficient 1.
+            --total_evolving_time 3. --ODE_tolerance 1e-5 --plot_domain_size 10. --diffusion_coefficient 1.\
+            --method NWGF
